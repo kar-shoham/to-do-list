2fceef27e814081b572678fb75e395572a2aa51d
Shoham <kar.shoham.25@gmail.com>
2023-05-10 11:41:01 +0530

    changed cors settings

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     credentials: true
 }))
 
